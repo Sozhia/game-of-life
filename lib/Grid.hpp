1ed9b55a146e4d160c25e3ac33fa0b8e1a3df40b
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2022-02-25 09:31:18 +0000

    Setup a 0
    Se han añadido los métodos solicitados en el informe de prácticas. Por confirmar el uso de la clase State (si se cambia, si hay que crear una clase, ...)

--- lib/Grid.hpp
+++ lib/Grid.hpp
@@ -1,7 +1,14 @@
+#ifndef _GRIDHPP_
+#define _GRIDHPP_
+
 #include <iostream>
 
 class Cell;
 
 class Grid{
-
+ public:
+  const Cell& Grid::getCell(int, int) const;
+  void Grid::nextGeneration();
 };
+
+#endif
