49f1b4638344c49e53f2e4a0b652a0dd8480ab6b
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2022-02-26 09:49:53 +0000

    Finaliza la declaración de la clase Cell

--- lib/Cell.hpp
+++ lib/Cell.hpp
@@ -9,12 +9,15 @@ class Grid;
 
 class Cell{
  public:
+   Cell();
+   Cell(posx, posy, state);
    State getState() const;
    State setState(State);
    void updateState();
    int neighbors(const Grid&);
    ostream& operator<<(ostream&, const Cell&);
-
+   ~Cell();
+   
  private:
   int posx_, posy_;
   State state_;
