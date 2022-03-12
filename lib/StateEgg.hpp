274f5b9074574057d916c9c27da978f1b8abd0f8
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2022-03-12 07:11:02 +0000

    Declaración de StateEgg
    Se han declarado los métodos de la superclase

--- lib/StateEgg.hpp
+++ lib/StateEgg.hpp
@@ -1,10 +1,16 @@
 #ifndef _STATEEGGHPP_
 #define _STATEEGGHPP_
 
+#include <iostream>
 #include "IState.hpp"
 
 class StateEgg : public State{
- public
+ public:
+  StateEgg();
+  int neighbors(const Grid&, int, int);
+  State* nextState();
+  char getState() const;
+  friend ostream& operator<<(ostream&, const State&);
 };
 
 #endif
