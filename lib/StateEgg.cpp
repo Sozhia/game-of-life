c4750cdaf657d2026f8f5d21a2de6914c850ea5e
Miqueas (Sozhia) García González <coding4canary@gmail.com>
2022-03-12 07:09:59 +0000

    Creación de source file para clase StateEgg
    Se ha creado el fichero que tiene la definición de la clase StateEgg

---
+++ lib/StateEgg.cpp
@@ -0,0 +1,23 @@
+#ifndef _STATEEGGCPP_
+#define _STATEEGGCPP_
+
+#include "StateEgg.hpp"
+
+
+StateEgg::StateEgg() {
+    
+}
+
+int StateEgg::neighbors(const Grid&, int, int) {
+    
+}
+
+State* StateEgg::nextState() {
+    
+}
+
+char StateEgg::getState() const {
+    
+}
+
+#endif
