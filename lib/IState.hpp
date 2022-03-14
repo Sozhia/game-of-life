66ff48debf3459b486ec1bee2a98e6f1c226aced
Miqueas García González <coding4canary@gmail.com>
2022-03-14 07:02:12 +0000

    Finalizada declaración de interfaz IState

--- lib/IState.hpp
+++ lib/IState.hpp
@@ -6,8 +6,12 @@
 
 class IState{
  public:
-  virtual IState* nextState(std::vector<IState>) = 0;
+  virtual IState* nextState(std::vector<IState&>) = 0;
   virtual char getState() const =0;
+
+ private:
+  virtual int countStates(char, std::vector<IState&>) = 0;
+  virtual IState* findState(char, std::vector<IState&>) = 0;
 };
 
 #endif
