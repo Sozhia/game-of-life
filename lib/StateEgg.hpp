6743c78dd0e9046978fead024618717fbf61a4bd
Miqueas García González <coding4canary@gmail.com>
2022-03-14 07:03:02 +0000

    Finalizada declaración de clase StateEgg

--- lib/StateEgg.hpp
+++ lib/StateEgg.hpp
@@ -6,8 +6,12 @@
 class StateEgg : public IState{
 
  public:
-  IState* nextState(std::vector<IState>);
+  IState* nextState(std::vector<IState&>);
   char getState();
+
+ private:
+  int countStates(char, std::vector<IState&>);
+  IState* findState(char, std::vector<IState&>);
 };
 
 #endif
