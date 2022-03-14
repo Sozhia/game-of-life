3ca314c98a7ba8b25b83f68049dce28027b7ddd0
Miqueas García González <coding4canary@gmail.com>
2022-03-14 07:03:55 +0000

    Finalizada declaración de clase StatePupa

--- lib/StatePupa.hpp
+++ lib/StatePupa.hpp
@@ -3,11 +3,15 @@
 
 #include "IState.hpp"
 
-class StatePupa : public IState{
+class StateLarva : public IState{
 
  public:
-  IState* nextState(std::vector<IState>);
+  IState* nextState(std::vector<IState&>);
   char getState();
+
+ private:
+  int countStates(char, std::vector<IState&>);
+  IState* findState(char, std::vector<IState&>);
 };
 
 #endif
