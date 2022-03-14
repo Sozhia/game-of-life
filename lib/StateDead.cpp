6882bdf56659d191d3780cab3134eb99d388ec09
Miqueas García González <coding4canary@gmail.com>
2022-03-14 07:02:40 +0000

    Finalizada definición de clase StateDead

---
+++ lib/StateDead.cpp
@@ -0,0 +1,39 @@
+#ifndef _STATEDEADCPP_
+#define _STATEDEADCPP_
+
+#include "StateDead.hpp"
+
+IState* StateDead::nextState(std::vector<IState&> states_collection) {
+  int num_adult_states = 0;
+
+  num_adult_states = countStates('A', states_collection);
+
+  if (num_adult_states >= 2){
+    return findState('E', states_collection);
+  } else{
+    return findState('D', states_collection);
+  } 
+
+}
+
+int StateDead::countStates(char letter, std::vector<IState&> states_collection) {
+  int counter = 0;
+  for (int i = 0; i < states_collection.size(); i++)
+    (states_collection[i].getState() == letter)? counter++ : 0;
+  
+  return counter;
+}
+
+IState* StateDead::findState(char letter, std::vector<IState&> states_collection){
+  for (int i = 0; i < states_collection.size(); i++){
+    if (states_collection[i].getState() == letter){
+      return &states_collection[i];
+      break;
+    }
+  }
+
+}
+
+char StateDead::getState() {return 'D';}
+
+#endif
