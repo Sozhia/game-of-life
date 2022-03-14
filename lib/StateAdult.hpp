927330a7c572d4bb672c81de2266e034dc88f501
Miqueas García González <coding4canary@gmail.com>
2022-03-14 07:01:34 +0000

    Finalizada declaración de clase StateAdult

--- lib/StateAdult.hpp
+++ lib/StateAdult.hpp
@@ -6,8 +6,12 @@
 class StateAdult : public IState{
 
  public:
-  IState* nextState(std::vector<IState>);
+  IState* nextState(std::vector<IState&>);
   char getState();
+
+ private:
+  int countStates(char, std::vector<IState&>);
+  IState* findState(char, std::vector<IState&>);
 };
 
 #endif
